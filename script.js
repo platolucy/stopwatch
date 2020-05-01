af3b3bf0f37584989f0314875aa77d9c2b13459f
Savannah Seals <sav11_97@yahoo.com>
2020-04-30 23:28:07 -0500

    add css

---
+++ script.js
@@ -0,0 +1,9 @@
+// A "Start" button to start the stopwatch running.
+// A "Stop" button to pause/stop it.
+// A "Reset" button to reset the time back to 0.
+// The time display to show the number of seconds elapsed, rather than the actual time.
+
+const secondsElement = document.getElementById('seconds-elapsed')
+console.log(secondsElement);
+
+// A "Start" button to start the stopwatch running.
